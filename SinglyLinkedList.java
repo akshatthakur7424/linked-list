9320427d8e92855b16ea3b897aa5b0006ec79f7b
Akshat Singh Thakur <developerakshatsingh@gmail.com>
2025-09-06 11:42:56 +0530

    singly linked list basic operations implemenation

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -13,7 +13,7 @@ public class SinglyLinkedList {
         }
     }
 
-    // add node - first and last
+    // add node - first, last and between
     public void addFirst(String data) {
         Node newNode = new Node(data);
 
@@ -28,7 +28,7 @@ public class SinglyLinkedList {
     }
 
     public void addLast(String data) {
-        Node newNode = new Node(data);
+        Node newNode = new Node(data); // creating a new node
 
         // check for null linkedlist
         if (head == null) {
@@ -43,7 +43,25 @@ public class SinglyLinkedList {
         tempNode.next = newNode;
     }
 
-    // print linkedlist
+    public void addAfter(String data, int nodeNumber) {
+        // creating a new node
+        Node newNode = new Node(data);
+
+        // temporary pointer variables of node type
+        Node next = head;
+        Node prev = null;
+        int count = 0;
+
+        while (count != nodeNumber) {
+            count++;
+            prev = next;
+            next = next.next;
+        }
+        prev.next = newNode;
+        newNode.next = next;
+    }
+
+    // reading/ traversing/ printing linkedlist
     public void printLinkedList() {
         Node tempNode = head;
         while (tempNode != null) {
@@ -55,27 +73,136 @@ public class SinglyLinkedList {
         System.out.println();
     }
 
+    // deletion - first, last and between
+    public void deleteFirst() {
+        head = head.next;
+    }
+
+    public void deleteLast() {
+        // empty linked list
+        if (head == null) {
+            return;
+        }
+
+        // linked list with single node case
+        if (head.next == null) {
+            head = null;
+        }
+
+        // linlked list with more than one node case
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+
+    }
+
+    public void deleteAfter(int nodeNumber) {
+        // empty linked list case
+        if (head == null) {
+            return;
+        }
+
+        // single node linked list case
+        if (head.next == null) {
+            head = null;
+        }
+
+        // more than one node linked list case
+        Node prev = null;
+        Node current = head;
+        int count = 0;
+
+        // going to the deletion node
+        while (count != nodeNumber) {
+            count++;
+            prev = current;
+            current = current.next;
+        }
+        prev.next = current.next;
+        current.next = null;
+    }
+
+    // updation of element
+    public void updateElement(int nodeNumber, String data) {
+        // empty linked list case
+        if (head == null) {
+            return;
+        }
+
+        Node temp = head;
+        int counter = 1;
+
+        while (counter != nodeNumber) {
+            counter++;
+            temp = temp.next;
+        }
+        temp.data = data;
+    }
+
+    // searching element
+    public void searchElement(String data) {
+        // empty linked list case
+        if (head == null) {
+            return;
+        }
+
+        // searching node through data
+        Node temp = head;
+        int counter = 1;
+        if (temp.data == data) {
+            System.out.println("Node with data: " + temp.data + " Found at position : " + counter);
+            return;
+        }
+        while (temp.next != null) {
+            counter++;
+            temp = temp.next;
+            if (temp.data == data) {
+                System.out.println("Node with data: " + temp.data + " Found at position : " + counter);
+                return;
+            }
+        }
+        System.out.println("Node with data: " + data + " Not Found on Linked List ");
+
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
 
         // adding elements at first
-        list.addFirst("A");
-        list.addFirst("B");
-        list.addFirst("C");
+        list.addFirst("G");
+        list.addFirst("F");
+        list.addFirst("E");
         list.addFirst("D");
+        list.addFirst("C");
+        list.addFirst("B");
+        list.addFirst("A");
         list.printLinkedList(); // print
+
+        // list.addAfter("NewNode", 5);
+        // list.printLinkedList(); // print
 
         // adding elements at last
-        list.addLast("E");
-        list.addLast("F");
-        list.addLast("G");
-        list.addLast("H");
-        list.printLinkedList(); // print
+        // list.addLast("E");
+        // list.addLast("F");
+        // list.addLast("G");
+        // list.addLast("H");
+        // list.printLinkedList(); // print
 
         // deleting elements from fist
-        list.printLinkedList(); // print
+        // list.deleteFirst();
+        // list.printLinkedList(); // print
+        // list.deleteLast();
+        // list.printLinkedList(); // print
+        // list.deleteAfter(4);
+        // list.printLinkedList();
 
-        // deleting elements from last
-        list.printLinkedList(); // print
+        // updating element
+        // list.updateElement(3, "Updated");
+        // list.printLinkedList(); // print
+
+        // searching element
+        list.searchElement("C");
     }
 }
