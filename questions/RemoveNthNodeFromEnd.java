1cd6a46ceb1bb44013520994d4ba1177a0127018
Akshat Singh Thakur <developerakshatsingh@gmail.com>
2025-12-31 12:57:21 +0530

    remove nth node from the end of LL ( optimal solution )

--- questions/RemoveNthNodeFromEnd.java
+++ questions/RemoveNthNodeFromEnd.java
@@ -12,6 +12,7 @@
  * }
  */
 class Solution {
+    // brute force approach
     public ListNode removeNthFromEnd(ListNode head, int n) {
         //  traversing and counting the length of the linkedlist
         int count = 0;
@@ -41,6 +42,39 @@ class Solution {
         
         ListNode delNode = temp.next;
         temp.next = temp.next.next;
+        delNode.next = null;
+
+        return head;
+    }
+
+
+    // optimal approach
+     public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+        int count = n;
+
+        // taking fast n steps forward
+        while (count > 0) {
+            fast = fast.next;
+            count--;
+        }
+
+        // handling edge case - first node deletion
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        // taking slow before deletion node 
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // deleting nth node 
+        ListNode delNode = slow.next;
+        slow.next = slow.next.next;
         delNode.next = null;
 
         return head;
